b54f4874772e94f92b01ba656736c9dc404ec7b7
nakamurakazuya <nakamurakazuya@team-lab.com>
2018-05-23 02:39:32 +0900

    初期設定

---
+++ betterlife/src/main/java/bl/BetterlifeApplication.java
@@ -0,0 +1,12 @@
+package bl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterlifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterlifeApplication.class, args);
+    }
+}
